90a3cf1b7b62d1c8c6993209c1ad7733916a42a2
jshwa <32717724+jshwa@users.noreply.github.com>
2017-11-19 23:47:23 -0600

    added #print_ride to CLI that outputs all the ride info

--- lib/disney_rides/cli.rb
+++ lib/disney_rides/cli.rb
@@ -12,7 +12,15 @@ class DisneyRides::CLI
       puts "3. See fastpass rides"
       puts "4. Print all rides"
       input = gets.strip.to_i
+    end
 
-      
+    def print_ride(ride)
+      puts "-----------#{ride.name}-----------"
+      puts "Disney:           #{ride.location}"
+      puts "Park:             #{ride.park}"
+      puts "Thrill Level:     #{ride.thrill_lvl}"
+      puts "Hours:            #{ride.hours}"
+      puts "Fastpass:         #{ride.fastpass}"
+      puts "Description:      #{ride.description}"
     end
 end
