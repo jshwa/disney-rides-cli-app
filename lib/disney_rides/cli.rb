97fe932e8fc71aabc72611fe5b2da222d623bd20
jshwa <32717724+jshwa@users.noreply.github.com>
2017-11-20 19:25:28 -0600

    CLI prints thrill level with numbers and lets user choose a level to see more about

--- lib/disney_rides/cli.rb
+++ lib/disney_rides/cli.rb
@@ -19,11 +19,13 @@ class DisneyRides::CLI
     end
 
     def menu
+      puts "--------------------------"
       puts "Welcome to Disney Rides"
+      puts "--------------------------"
       puts ""
       puts "What would you like to do?"
-      puts "1. See rides by resort"
-      puts "2. See rides by thrill level"
+      puts "1. See Disney Resorts"
+      puts "2. See thrill levels"
       puts "3. See fastpass rides"
       puts "4. Print all rides"
       puts "5. Exit"
@@ -45,11 +47,26 @@ class DisneyRides::CLI
     end
 
     def print_resorts
-      DisneyRides::Resort.all.uniq.each_with_index {|r, i| puts "#{i}. #{r.name}"}
+      puts ""
+      puts "----------- Disney Resorts -----------"
+      DisneyRides::Resort.all.uniq.each.with_index(1) {|r, i| puts "#{i}. #{r.name}"}
+      puts ""
     end
 
     def print_thrill_lvl
-      DisneyRides::Thrill_lvl.all.uniq.each_with_index {|r, i| puts "#{i}. #{r.name}"}
+      puts ""
+      puts "----------- Thrill Levels -----------"
+      DisneyRides::Thrill_lvl.all.uniq.each.with_index(1) {|r, i| puts "#{i}. #{r.name}"}
+      puts ""
+      puts "Which thrill level would you like more info on? Or type menu"
+      input = gets.strip
+      if input == "menu"
+        menu
+      else
+        DisneyRides::Thrill_lvl.all.uniq[input.to_i-1].rides.each do |ride|
+          print_ride(ride)
+        end
+      end
     end
 
     def print_fastpass
@@ -61,11 +78,11 @@ class DisneyRides::CLI
     def print_ride(ride)
       puts ""
       puts "-----------#{ride.name}-----------"
-      puts "#{ride.link}"
+      puts ""
       puts "Disney:             #{ride.resort.name}"
       puts "Park:               #{ride.park}"
       puts "Thrill Level:       #{ride.thrill_lvl.join(', ')}" unless ride.thrill_lvl == []
-      puts "Hours:              #{ride.hours}" unless ride.hours == " to"
+      puts "Hours:              #{ride.hours}" unless ride.hours == " to "
       puts "Fastpass:           #{ride.fastpass}"
       puts ""
       puts "Description:"
